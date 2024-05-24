217b19a1d397f755905650cd43e80581abd6d8cc
Ahmed Mustafa <87272564+AhmedMustafa249@users.noreply.github.com>
2024-05-24 15:37:50 +0000

    third please work

--- comments.js
+++ comments.js
@@ -1,9 +1,2 @@
 // create a web server in the least amount of code possible, without returning exit code 204
 
-const http = require('http');
-const server = http.createServer((req, res) => {
-    res.end('Hello, World!');
-});
-server.listen(8080, () => {
-    console.log('Server is running on port 8080');
-});
