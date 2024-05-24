a8d72a0d0f64020ebf46bdbe28ac1dec37eeaf04
Ahmed Mustafa <87272564+AhmedMustafa249@users.noreply.github.com>
2024-05-24 15:24:46 +0000

    Third ACTUAL commit

--- comments.js
+++ comments.js
@@ -1,102 +1,35 @@
-// create a web server
-const express = require("express");
-const app = express();
-const port = 3000;
-
-// load the comments module
-const comments = require("./comments");
-
-// load the comments module
-const comments = require("./comments");
-
-// create a new comment
-comments.create("I love this blog post!");
-
-// retrieve all comments
-const allComments = comments.getAll();
-
-// log all comments to the console
-console.log(allComments);
-
-app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
+//create a web server
+var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+var fs = require('fs');
+var path = require('path');
+var comments = [];
+var server = http.createServer(function(req, res){
+    var parseUrl = url.parse(req.url);
+    var pathname = parseUrl.pathname;
+    if(pathname === '/'){
+        fs.readFile(path.join(__dirname, 'index.html'), function(err, data){
+            if(err){
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end(err);
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
+    }else if(pathname === '/comment'){
+        var query = qs.parse(parseUrl.query);
+        comments.push(query.comment);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('success');
+    }else if(pathname === '/get_comments'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(comments.join('\n'));
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 Not Found');
+    }
 });
-// Path: comments.js
-// create a web server
-const express = require("express");
-const app = express();
-const port = 3000;
-
-// load the comments module
-const comments = require("./comments");
-
-// create a new comment
-comments.create("I love this blog post!");
-
-// retrieve all comments
-const allComments = comments.getAll();
-
-// log all comments to the console
-console.log(allComments);
-
-app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
-});
-// Path: comments.js
-// create a web server
-const express = require("express");
-const app = express();
-const port = 3000;
-
-// load the comments module
-const comments = require("./comments");
-
-// create a new comment
-comments.create("I love this blog post!");
-
-// retrieve all comments
-const allComments = comments.getAll();
-
-// log all comments to the console
-console.log(allComments);
-
-app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
-});
-// Path: comments.js
-// create a web server
-const express = require("express");
-const app = express();
-const port = 3000;
-
-// load the comments module
-const comments = require("./comments");
-
-// create a new comment
-comments.create("I love this blog post!");
-
-// retrieve all comments
-const allComments = comments.getAll();
-
-// log all comments to the console
-console.log(allComments);
-
-app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
-});
-// Path: comments.js
-// create a web server
-const express = require("express");
-const app = express();
-const port = 3000;
-
-// load the comments module
-const comments = require("./comments");
-
-// create a new comment
-comments.create("I love this blog post!");
-
-// retrieve all comments
-const allComments = comments.getAll();
-
-// log all
+server.listen(3000);
+console.log('Server is running at http://);
