5172c18356c8f8328c9c4b175dd04c610340fe3c
Ahmed Mustafa <87272564+AhmedMustafa249@users.noreply.github.com>
2024-05-24 15:29:29 +0000

    Third ACTUAL ACTUAL commit

--- comments.js
+++ comments.js
@@ -1,35 +1,27 @@
-//create a web server
-var http = require('http');
-var url = require('url');
-var qs = require('querystring');
-var fs = require('fs');
-var path = require('path');
-var comments = [];
-var server = http.createServer(function(req, res){
-    var parseUrl = url.parse(req.url);
-    var pathname = parseUrl.pathname;
-    if(pathname === '/'){
-        fs.readFile(path.join(__dirname, 'index.html'), function(err, data){
-            if(err){
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end(err);
-            }else{
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
-        });
-    }else if(pathname === '/comment'){
-        var query = qs.parse(parseUrl.query);
-        comments.push(query.comment);
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('success');
-    }else if(pathname === '/get_comments'){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(comments.join('\n'));
-    }else{
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('404 Not Found');
-    }
+// create a web server in the least amount of code possible, without returning exit code 204
+// 1. require http module
+// 2. require url module
+// 3. create a server that listens on port 3000
+// 4. when a request is made, parse the url
+// 5. if the url is /comments, return a JSON object with some comments
+// 6. if the url is /users, return a JSON object with some users
+// 7. if the url is neither /comments nor /users, return a 404 status code with a message
+
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  if (parsedUrl.pathname === '/comments') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ comments: ['comment1', 'comment2'] }));
+  } else if (parsedUrl.pathname === '/users') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ users: ['user1', 'user2'] }));
+  } else {
+    res.writeHead(404);
+    res.end('404 Not Found');
+  }
 });
-server.listen(3000);
-console.log('Server is running at http://);
+
+server.listen(3000, () => console.log('Server is listening on port 3000'));
