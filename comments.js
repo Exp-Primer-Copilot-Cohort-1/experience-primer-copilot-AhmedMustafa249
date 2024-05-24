573ae54bcbdf6c04a021fa982bce1a29da35a5e1
Ahmed Mustafa <87272564+AhmedMustafa249@users.noreply.github.com>
2024-05-24 15:53:22 +0000

    sadasdasd

--- comments.js
+++ comments.js
@@ -1,32 +1,47 @@
 // create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const commentsPath = path.join(__dirname, 'comments.json');
+
+// middleware
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
 
-// Path: comments.js
-// create comments array
-var comments = [{name: 'John', message: 'Hello World!'}];
-
-// Path: comments.js
-// create get method
-app.get('/comments', function(req, res) {
-    res.send(comments);
+app.get('/comments', (req, res) => {
+  fs.readFile(commentsPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).end();
+      return;
+    }
+    res.send(data);
+  });
 });
 
-// Path: comments.js
-// create post method
-app.post('/comments', function(req, res) {
+app.post('/comments', (req, res) => {
+  fs.readFile(commentsPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).end();
+      return;
+    }
+    const comments = JSON.parse(data);
     comments.push(req.body);
-    res.send(comments);
+    fs.writeFile(commentsPath, JSON.stringify(comments), err => {
+      if (err) {
+        console.error(err);
+        res.status(500).end();
+        return;
+      }
+      res.send('success');
+    });
+  });
 });
 
-// Path: comments.js
-// create server to listen port 3000
-app.listen(3000, function() {
-    console.log('Server running at http://localhost:3000');
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
-
