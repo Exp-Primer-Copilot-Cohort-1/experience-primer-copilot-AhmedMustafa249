de57b479d31d4823768afb46c3fd90aecfc304ac
Ahmed Mustafa <87272564+AhmedMustafa249@users.noreply.github.com>
2024-05-24 15:19:08 +0000

    third commit

---
+++ comments.js
@@ -0,0 +1,102 @@
+// create a web server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// load the comments module
+const comments = require("./comments");
+
+// load the comments module
+const comments = require("./comments");
+
+// create a new comment
+comments.create("I love this blog post!");
+
+// retrieve all comments
+const allComments = comments.getAll();
+
+// log all comments to the console
+console.log(allComments);
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
+// Path: comments.js
+// create a web server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// load the comments module
+const comments = require("./comments");
+
+// create a new comment
+comments.create("I love this blog post!");
+
+// retrieve all comments
+const allComments = comments.getAll();
+
+// log all comments to the console
+console.log(allComments);
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
+// Path: comments.js
+// create a web server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// load the comments module
+const comments = require("./comments");
+
+// create a new comment
+comments.create("I love this blog post!");
+
+// retrieve all comments
+const allComments = comments.getAll();
+
+// log all comments to the console
+console.log(allComments);
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
+// Path: comments.js
+// create a web server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// load the comments module
+const comments = require("./comments");
+
+// create a new comment
+comments.create("I love this blog post!");
+
+// retrieve all comments
+const allComments = comments.getAll();
+
+// log all comments to the console
+console.log(allComments);
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
+// Path: comments.js
+// create a web server
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// load the comments module
+const comments = require("./comments");
+
+// create a new comment
+comments.create("I love this blog post!");
+
+// retrieve all comments
+const allComments = comments.getAll();
+
+// log all
